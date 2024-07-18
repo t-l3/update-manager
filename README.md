7d9ec14b5b066ad51c349c1f58d2060386a95a11
t-l3 <t.l3.gard@gmail.com>
2024-07-18 18:07:30 +0100

    Version detection logic

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ apps:
       owner: [string] Linux file owner string for top level install directory and sub-files (i.e root or root:root)
       mode: [string] Linux file mode representation for top level install directory (i.e 755)
     versioning-checks:
-      installed: [string] An executable command to check the locally installed version (i.e jq '.version' .discord/resources/build_info.json)
+      installed: [string] An executable command to check the locally installed version (i.e jq -r '.version' /opt/discord/resources/build_info.json)
       latest: [string] An executable command to check the remotely latest available version (i.e curl -s 'https://discord.com/api/download?platform=linux&format=tar.gz' | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | head -n 1;)
     pre-install-script: [string] Script to execute in bash before install
     post-install-script: [string] Script to execute in bash after install
--- internal/manager/app.go
+++ internal/manager/app.go
@@ -4,20 +4,44 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/t-l3/update-manager/internal/config"
 
 	"github.com/0xAX/notificator"
 )
 
-func UpdateApplication(app* config.App) {
-	logger := log.New(os.Stdout, fmt.Sprintf("app-manager-%s  ", app.Name), log.Ldate | log.Ltime | log.Lmsgprefix)
+func UpdateApplication(app *config.App) {
+	logger := log.New(os.Stdout, fmt.Sprintf("app-manager-%s  ", app.Name), log.Ldate|log.Ltime|log.Lmsgprefix)
 	logger.Printf("Checking '%s'...", app.Name)
 
 	notify := notificator.New(notificator.Options{
-    DefaultIcon: app.Icon,
-    AppName:     "update-manager",
-  })
+		DefaultIcon: app.Icon,
+		AppName:     "update-manager",
+	})
 
-  notify.Push("Update found", fmt.Sprintf("Installing update for %s", app.Name), app.Icon, notificator.UR_NORMAL)
+	installedCheckProc := exec.Command("/usr/bin/bash", "-c", app.VersioningChecks.Installed)
+	installedCheckOutput, err := installedCheckProc.Output()
+	if err != nil {
+		logger.Println("Failed to run installed version check process\n", err)
+		return
+	}
+	installedVersionString := strings.Trim(string(installedCheckOutput), " \t\n")
+
+	latestCheckProc := exec.Command("/usr/bin/bash", "-c", app.VersioningChecks.Latest)
+	latestCheckOutput, err := latestCheckProc.Output()
+	if err != nil {
+		logger.Println("Failed to run latest version check process\n", err)
+		return
+	}
+	latestVersionString := strings.Trim(string(latestCheckOutput), " \t\n")
+
+	if installedVersionString == latestVersionString {
+		logger.Printf("The latest version of %s is currently installed", app.Name)
+		return
+	} else {
+		logger.Printf("Update for %s found\n", app.Name)
+		notify.Push("Update found", fmt.Sprintf("Installing update for %s", app.Name), app.Icon, notificator.UR_NORMAL)
+	}
 }
